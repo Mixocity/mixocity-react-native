1fda9319a6fc869f8311d6f7a52dbb452b918f2b
Mixocity <mixocityapp@gmail.com>
2021-11-17 11:50:01 -0500

    First Commit

---
+++ MixocityProject/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+const App = () => 
+{
+  return (
+    <SafeAreaView>
+  <view>
+    <text>Mixocity</text>
+  </view>
+  </SafeAreaView>
+  );
+
+}
+
+
+
+export default App;
